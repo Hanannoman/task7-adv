d7715ce8b31cdab10d09baef4dc8683a4a03b802
hanannoman <hanannoman392003123@gmail.com>
2026-08-27 20:37:08 +0300

    pages-demo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+output: 'export',
+  basePath: '/task7-adv', // تأكدي أن يكون نفس اسم مستودعك على غيت هاب تماماً
+  assetPrefix: '/task7-adv/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
